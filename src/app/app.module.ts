d4b5350c86e730aa7492a711964c24b4b9f2e1e4
j-bermejo <j.bermejo.rodriguez1@gmail.com>
2022-10-10 14:22:40 +0200

    Vista home creada, implementar funcionalidad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { GameComponent } from './components/game/game.component';
+
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'autoclicker', component:GameComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
