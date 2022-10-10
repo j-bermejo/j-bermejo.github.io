dadf67434e6ff8636c3e9519be902b463600d462
j-bermejo <j.bermejo.rodriguez1@gmail.com>
2022-10-10 15:06:23 +0200

    Vista home con funcionalidad implementada

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Renderer2 } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public registeredUsers = ['Squid', 'Javi', 'BBVA']
+  public userName:string="";
+  public errorMessage:string="";
+  public incorrectUser:boolean=false;
 
-  ngOnInit() {
+@ViewChild('userInput') isWrongUser:any;
+@ViewChild('placeholder') placeholder:any;
+
+  constructor(private render: Renderer2, private router:Router) { }
+
+  ngOnInit() {console.log(this.userName);}
+
+  public saveUser(userInput: string) {
+    this.userName = userInput;
+
+  }
+
+  public validateUser(){
+    if(this.registeredUsers.includes(this.userName)){
+      this.incorrectUser = false;
+      this.router.navigate(['/autoclicker']);
+    }
+    else if(this.userName == ''){
+      this.incorrectUser = true;
+      this.errorMessage = 'Required field';
+      this.render.setStyle(this.isWrongUser.nativeElement, 'border-color', 'red');
+      this.render.setStyle(this.placeholder.nativeElement, 'color', 'red');
+    }
+    else{
+      this.incorrectUser = true;
+      this.errorMessage = 'Invalid User'
+      this.render.setStyle(this.isWrongUser.nativeElement, 'border-color', 'red');
+      this.render.setStyle(this.placeholder.nativeElement, 'color', 'red');
+      this.isWrongUser.nativeElement.value='';
+    }
   }
 
 }
