a7c9dd1a2aa7a4bfa3db88e58137dc1638da58de
j-bermejo <j.bermejo.rodriguez1@gmail.com>
2022-10-11 16:48:02 +0200

    Funcion megaClicker y mejora del autoClicker implementadas

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -7,87 +7,130 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
-
+  
   @ViewChild('autoMergerBtn') autoMergerBtn: any;
+  @ViewChild('autoMergerUpgradeBtn') autoMergerUpgradeBtn: any;
+  @ViewChild('megaClickerBtn') megaClickerBtn: any;
 
   public userName = this.route.snapshot.paramMap.get('userName');
   public score: number = 0;
+  public clickPower: number = 1;
+
   public autoClickers: number = 0;
   public autoClickerCost: number = 50;
   public canBuyAutoClicker: boolean = false;
+
+  public autoClickerUpgrade: number = 0;
+  public autoClickerUpgradeCost: number = 2000;
+  public canBuyAutoClickerUpgrade: boolean = false;
+
   public scoreView: string = `${this.score}`;
-  public upgradeCostView: string = `${this.autoClickerCost}`;
-  public hasAnyUpgrade: boolean = false;
+  public autoClickerCostView: string = `${this.autoClickerCost}`;
+  public autoClickerUpgradeCostView: string = '2k';
+  public megaClickerCostView: string = '5k';
+
+  public megaClicker: number = 0;
+  public megaClickerCost: number = 5000;
+  public canBuyMegaClicker: boolean = false; 
 
   constructor(private route: ActivatedRoute, private router: Router, private render: Renderer2) { }
 
   ngOnInit() { }
 
+  public buyMegaClicker() {
+    if (this.canBuyMegaClicker) {
+      this.score = this.score - this.megaClickerCost;
+      this.megaClicker += 1;
+      this.clickPower = Math.pow(10, this.megaClicker);
+      this.megaClickerCost = this.megaClickerCost + this.megaClickerCost * this.megaClicker;
+
+      this.megaClickerCostView = this.formatVariables(this.megaClickerCost, this.megaClickerCostView);
+      this.upgradeButtonsStatus();
+    }
+  }
+
   public exit() {
     this.router.navigate(['/']);
   }
 
-  public addSingleMerge() {
-    this.score += 1;
-    this.formatScore();
-    this.formatPrice();
+  public addMerge() {
+    this.score += this.clickPower;
 
-    if (this.score >= this.autoClickerCost) this.canBuyAutoClicker = true;
-    else this.canBuyAutoClicker = false;
+    this.scoreView = this.formatVariables(this.score, this.scoreView);
+    this.upgradeButtonsStatus();
+
+    console.log('AUTO: '+this.autoClickers + this.autoClickerUpgrade * 5+ '\nPOWER: '+this.clickPower);
+    
   }
 
-  public buyAutoMerger() {
+  public buyAutoClicker() {
     if (this.canBuyAutoClicker) {
-      this.hasAnyUpgrade = true;
-      this.score=this.score-this.autoClickerCost;
-      setInterval(() => {
-        this.score += 1;
-        this.formatScore();
-        this.formatPrice();
-
-        if (this.score >= this.autoClickerCost) {
-          this.canBuyAutoClicker = true;
-          this.render.setStyle(this.autoMergerBtn.nativeElement, 'background-color', 'skyblue');
-        }
-      }, 100);
-
-      this.canBuyAutoClicker = false;
+      this.score = this.score - this.autoClickerCost;
       this.autoClickers += 1;
       this.autoClickerCost = this.autoClickerCost + this.autoClickerCost * this.autoClickers;
+
+      setInterval(() => {
+        if (this.autoClickerUpgrade != 0) this.score += this.autoClickers + this.autoClickerUpgrade * 10;
+        else this.score += 1;
+
+        this.scoreView = this.formatVariables(this.score, this.scoreView);
+        this.autoClickerCostView = this.formatVariables(this.autoClickerCost, this.autoClickerCostView);
+        this.upgradeButtonsStatus();
+      }, 100);
+    }
+  }
+
+  public buyAutoClickerUpgrade() {
+    if (this.canBuyAutoClickerUpgrade) {
+      this.score = this.score - this.autoClickerUpgradeCost;
+      this.autoClickerUpgrade += 1;
+      this.autoClickerUpgradeCost = this.autoClickerUpgradeCost + this.autoClickerUpgradeCost * this.autoClickerUpgrade;
+
+      this.autoClickerUpgradeCostView = this.formatVariables(this.autoClickerUpgradeCost, this.autoClickerUpgradeCostView);
+      this.upgradeButtonsStatus();
+    }
+  }
+
+ //
+
+  public upgradeButtonsStatus() {
+    if (this.score >= this.autoClickerCost) {
+      this.canBuyAutoClicker = true;
+      this.render.setStyle(this.autoMergerBtn.nativeElement, 'background-color', 'skyblue');
+    } else {
+      this.canBuyAutoClicker = false;
       this.render.setStyle(this.autoMergerBtn.nativeElement, 'background-color', 'rgb(50, 50, 50)');
     }
-  }
 
-  public formatScore() {
-    switch (true) {
-      case this.score < 1000:
-        this.scoreView = `${this.score}`;
-        break;
+    if (this.score >= this.autoClickerUpgradeCost) {
+      this.canBuyAutoClickerUpgrade = true;
+      this.render.setStyle(this.autoMergerUpgradeBtn.nativeElement, 'background-color', 'skyblue');
+    } else {
+      this.canBuyAutoClickerUpgrade = false;
+      this.render.setStyle(this.autoMergerUpgradeBtn.nativeElement, 'background-color', 'rgb(50, 50, 50)');
+    }
 
-      case this.score >= 1000:
-        this.scoreView = Math.round((this.score / 1000)*100)/100 + 'k';
-        break;
-
-      case this.score >= 1000000:
-        this.scoreView = Math.round(this.score / 1000000) + 'M';
-        break;
+    if (this.score >= this.megaClickerCost) {
+      this.canBuyMegaClicker = true;
+      this.render.setStyle(this.megaClickerBtn.nativeElement, 'background-color', 'skyblue');
+    } else {
+      this.canBuyMegaClicker = false;
+      this.render.setStyle(this.megaClickerBtn.nativeElement, 'background-color', 'rgb(50, 50, 50)');
     }
   }
 
-  public formatPrice() {
-    switch (true) {
-      case this.autoClickerCost < 1000:
-        this.upgradeCostView = `${this.autoClickerCost}`;
-        break;
+  public formatVariables(variableToFormat: number, variableFormatted: string) {
+    const suffixes = ["", "k", "M", "B", "T","C","Q","S","MM","MB","MT","MC","MQ","MS"];
+    let suffixIndex = 0;
 
-      case this.autoClickerCost >= 1000:
-        this.upgradeCostView = Math.round((this.autoClickerCost / 1000)*100)/100 + 'k';
-        break;
+    while (variableToFormat >= 1000) {
+      variableToFormat /= 1000;
+      suffixIndex++;
+    } 
 
-      case this.autoClickerCost >= 1000000:
-        this.upgradeCostView = Math.round(this.autoClickerCost / 1000000) + 'M';
-        break;
-    }
+    variableFormatted = variableToFormat.toPrecision(3);
+    variableFormatted += suffixes[suffixIndex];
+
+    return variableFormatted;
   }
-
 }
